f1e14dd76dfa3a2f1c0b0f4498b74818001618db
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-07-17 15:15:39 -0400

    Update: binary-search - submission-2

--- Data Structures & Algorithms/binary-search/submission-2.java
+++ Data Structures & Algorithms/binary-search/submission-2.java
@@ -1,15 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
+        int l = 0, r = nums.length;
+        while (l < r){
+            int mid = l + ((r - l) / 2);
             if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (nums[mid] < target) l = mid + 1;
+            if (nums[mid] > target) r = mid;
         }
-
         return -1;
     }
 }
