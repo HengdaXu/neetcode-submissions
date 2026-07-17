c7bcbcd96563b75275a33fe9484a50802e11fa86
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-07-17 13:52:41 -0400

    Update: longest-repeating-substring-with-replacement - submission-2

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -1,20 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        
+        int l = 0, maxF = 0, res = 0, local = 0;
+        for (int r = 0; r < s.length(); r++){
+            char c = s.charAt(r);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            
+            if (freq.get(c) > maxF) maxF = freq.get(c);
 
-        int res = 0, l = 0, max_f = 0;
-        for (int r=0; r < s.length(); r++){
-            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
-            max_f = Math.max(max_f, map.get(s.charAt(r)));
-
-            while (r - l + 1 - max_f > k){
-                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+            while (l < r && r - l + 1 - maxF > k){
+                char a = s.charAt(l);
+                freq.put(a, freq.get(a) - 1);
                 l++;
             }
 
             res = Math.max(res, r - l + 1);
         }
+
         return res;
-        
     }
 }
