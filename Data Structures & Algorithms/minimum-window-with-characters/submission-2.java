fb96f52fa5f9e5f6ccafae4e13439e37afd9024e
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-07-17 12:49:13 -0400

    Update: minimum-window-with-characters - submission-2

--- Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -1,35 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() == 0) return "";
-
-        HashMap<Character, Integer> mapT = new HashMap<>();
+        HashMap<Character, Integer> freq = new HashMap<>();
         for (int i = 0; i < t.length(); i++){
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+            freq.put(t.charAt(i), freq.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        int have = 0, need = mapT.size();
-        int resLen = Integer.MAX_VALUE;
-        int[] res = {-1, -1};
+        int needed = freq.size();
+        int have = 0;
         int l = 0;
-        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        int[] res = new int[]{-1, -1};
+        int minLen = Integer.MAX_VALUE;
         for (int r = 0; r < s.length(); r++){
-            char tmp = s.charAt(r);
-            mapS.put(tmp, mapS.getOrDefault(tmp, 0) + 1);
-            if (mapT.containsKey(tmp) && mapS.get(tmp).equals(mapT.get(tmp))) have++;
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            while (have == need){
-                if (r - l + 1 < resLen){
-                    res[0] = l;
-                    res[1] = r;
-                    resLen = r - l + 1;
+            if (freq.containsKey(c) && window.get(c).equals(freq.get(c))) have++;
+
+            while (have == needed){
+                if (r - l + 1 < minLen){
+                    minLen = r - l + 1;
+                    res[0] = l; res[1] = r;
                 }
-                
                 char left = s.charAt(l);
-                mapS.put(left, mapS.get(left) - 1);
-                if (mapT.containsKey(left) && mapS.get(left) < (mapT.get(left))) have--;
+                window.put(left, window.get(left) - 1);
+                if (freq.containsKey(left) && window.get(left) < freq.get(left)) have--;
                 l++;
             }
         }
-        return (resLen == Integer.MAX_VALUE) ? "" : s.substring(res[0], res[1] + 1);
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
     }
 }
