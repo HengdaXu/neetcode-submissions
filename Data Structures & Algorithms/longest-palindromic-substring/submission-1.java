b6499424c19b03f47312f371a2c04a98c3cb3815
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-07-18 18:46:06 -0400

    Update: longest-palindromic-substring - submission-1

--- Data Structures & Algorithms/longest-palindromic-substring/submission-1.java
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.java
@@ -1,19 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int resInd = 0, resLen = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
-
+        boolean[][] dp = new boolean[s.length() + 1][s.length() + 1];
+        dp[s.length()][s.length()] = true;
+        
+        int resLen = 0, resIdx = 0;
         for (int i = s.length() - 1; i >= 0; i--){
             for (int j = i; j < s.length(); j++){
-                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])){
+                if (s.charAt(i) == s.charAt(j) && (j - i  + 1<= 2 || dp[i + 1][j - 1])){
                     dp[i][j] = true;
-                    if (resLen < (j - i + 1)){
-                        resInd = i;
-                        resLen = j - i + 1;
-                    }
+                    resIdx = (j - i + 1) > resLen ? i : resIdx;
+                    resLen = Math.max(j - i + 1, resLen);
                 }
+                
             }
         }
-        return s.substring(resInd, resInd + resLen);
+        return resLen == 0 ? "" : s.substring(resIdx, resIdx + resLen);
+
     }
 }
