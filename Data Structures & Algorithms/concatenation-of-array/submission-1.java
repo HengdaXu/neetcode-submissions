6b00c04f276ac011096aca93b867c690f7819204
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-08-15 13:08:42 -0400

    Update: concatenation-of-array - submission-1

--- Data Structures & Algorithms/concatenation-of-array/submission-1.java
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[nums.length * 2];
-        for (int i = 0; i < nums.length; i++){
-            ans[i] = ans[i + nums.length] = nums[i];
+        int n = nums.length, i = 0;
+        int[] res = new int[n * 2];
+        for (int num: nums){
+            res[i] = res[i + n] = num;
+            i++;
         }
-        return ans;
+        return res;
     }
 }
