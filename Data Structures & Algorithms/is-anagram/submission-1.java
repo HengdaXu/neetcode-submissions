9cc4e64d0400b17d3ccfae78de805a9a220937bf
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-08-15 13:16:54 -0400

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.java
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -1,21 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
-            return false;
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        int[] countS = new int[26];
-        for (int i=0; i < s.length(); i++){
-            countS[s.charAt(i) - 'a']++;
-            countS[t.charAt(i) - 'a']--;
-        }
-
-        for (int c: countS){
-            if (c != 0){
-                return false;
-            }
-        }
-
-        return true;
-        
+        return Arrays.stream(count).allMatch(x -> x == 0);
     }
 }
