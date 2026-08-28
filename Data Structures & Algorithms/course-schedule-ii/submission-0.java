3d68a9ad216c8fbd0753bc384a8076c3893cd23b
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-08-28 13:55:52 -0400

    Update: course-schedule-ii - submission-0

--- Data Structures & Algorithms/course-schedule-ii/submission-0.java
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -1,32 +1,33 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-        List<List<Integer>> pre = new ArrayList<>();
+        List<Integer>[] neighbour = new ArrayList[numCourses];
+
         for (int i = 0; i < numCourses; i++){
-            pre.add(new ArrayList<>());
+            neighbour[i] = new ArrayList<>();
         }
 
-        for (int[] req: prerequisites){
-            indegree[req[1]]++;
-            pre.get(req[0]).add(req[1]);
+        for (int[] pre: prerequisites){
+            indegree[pre[0]]++;
+            neighbour[pre[1]].add(pre[0]);
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < numCourses; i++){
-            if (indegree[i] == 0) q.offer(i);
+            if (indegree[i] == 0) queue.offer(i);
         }
 
         int[] res = new int[numCourses];
-        int finish = 0;
-        while (!q.isEmpty()){
-            int curr = q.poll();
-            res[numCourses - finish - 1] = curr;
-            finish++;
-            for (int nei: pre.get(curr)){
-                if (--indegree[nei] == 0) q.offer(nei);
+        int taken = 0;
+        while (!queue.isEmpty()){
+            int course = queue.poll();
+            res[taken++] = course;
+            for (int nei: neighbour[course]){
+                indegree[nei]--;
+                if (indegree[nei] == 0) queue.offer(nei);
             }
         }
 
-        return (finish == numCourses) ? res : new int[]{};
+        return (taken == numCourses) ? res : new int[]{};
     }
 }
