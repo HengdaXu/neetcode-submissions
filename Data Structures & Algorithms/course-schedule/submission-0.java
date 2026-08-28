d693aaeeff52912e36ae4b272eb484305e75b3c7
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-08-28 13:35:02 -0400

    Update: course-schedule - submission-0

--- Data Structures & Algorithms/course-schedule/submission-0.java
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -1,38 +1,33 @@
-public class Solution {
+class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];           // 入度数组
-        List<List<Integer>> adj = new ArrayList<>();    // 邻接表（正向）
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+        // b - > a
+        int[] indegree = new int[numCourses];
+        HashMap<Integer, List<Integer>> neigh = new HashMap<>();
+
+        for (int i = 0; i < numCourses; i++){
+            neigh.put(i, new ArrayList<>());
+        }
+        for (int[] pre: prerequisites){
+            indegree[pre[0]]++;
+            neigh.get(pre[1]).add(pre[0]);
         }
 
-        // 构建正向图：b → a
-        for (int[] pre : prerequisites) {
-            int a = pre[0], b = pre[1];
-            adj.get(b).add(a);   // 从 b 指向 a
-            indegree[a]++;       // a 的入度 +1
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < indegree.length; i++){
+            if (indegree[i] == 0) queue.offer(i);
         }
 
-        // 初始化队列：入度为 0 的课程
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-        int finish = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            finish++;
-            for (int nei : adj.get(node)) {
+        int taken = 0;
+        while (!queue.isEmpty()){
+            int course = queue.poll();
+            taken++;
+            System.out.println(course);
+            for (int nei: neigh.get(course)){
                 indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    q.add(nei);
-                }
+                if (indegree[nei] == 0) queue.offer(nei);
             }
         }
 
-        return finish == numCourses;
+        return taken == numCourses;
     }
 }
