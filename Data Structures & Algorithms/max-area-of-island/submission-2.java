80e31d078afd2e044af5be1ccaaa32cecf8d5741
HengdaXu <71674436+HengdaXu@users.noreply.github.com>
2026-09-11 15:33:03 -0400

    Update: max-area-of-island - submission-2

--- Data Structures & Algorithms/max-area-of-island/submission-2.java
+++ Data Structures & Algorithms/max-area-of-island/submission-2.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int m, n;
     public int maxAreaOfIsland(int[][] grid) {
         m = grid.length;
@@ -8,22 +8,28 @@ class Solution {
         for (int r = 0; r < m; r++){
             for (int c = 0; c < n; c++){
                 if (grid[r][c] == 1){
-                    res = Math.max(res, dfs(grid, r, c));
+                    res = Math.max(dfs(grid, r, c), res);
+
                 }
             }
         }
-        return res; 
+        return res;
+
     }
 
     public int dfs(int[][] grid, int r, int c){
-        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0) return 0;
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0){
+            return 0;
+        }
 
         grid[r][c] = 0;
-        int res = 1;
+        int area  = 1;
+
         for (int[] dir: directions){
-            int nr = r + dir[0], nc = c + dir[1];
-            res += dfs(grid, nr, nc);
+            int dr = dir[0] + r;
+            int dc = dir[1] + c;
+            area += dfs(grid, dr, dc);
         }
-        return res;
+        return area;
     }
 }
